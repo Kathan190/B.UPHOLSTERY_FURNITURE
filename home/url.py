4580c1f1e5e0d315f9c021cd72a435dfa38df093
janmesh patel <janmesh04@gmail.com>
2020-12-15 17:04:50 +0530

    adding pages like beds,sofa,dining table,etc.

--- home/url.py
+++ home/url.py
@@ -3,5 +3,12 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("",views.index,name="Home")
+    path("",views.index,name="Home"),
+    path("beds",views.beds,name="beds"),
+    path("chestofdrawer",views.chestofdrawer,name="chestofdrawer"),
+    path("diningtable",views.diningtable,name="diningtable"),
+    path("dressingtable",views.dressingtable,name="dressingtable"),
+    path("sofa",views.sofa,name="sofa"),
+    path("wardrobs",views.wardrobs,name="wardrobs")
+        
 ]
