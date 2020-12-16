8f52ba3a555777a56151ba2ee87810f90260a123
iamkathan <shethkathan4@gmail.com>
2020-12-16 05:02:44 +0000

    Chnages done

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Sofa(models.Model):
+    id = models.CharField(primary_key=True, max_length=50)
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     description = models.CharField(max_length=1000)
