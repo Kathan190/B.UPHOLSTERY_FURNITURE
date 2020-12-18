66d44186ab36a57484b81d864997e4249d30458b
iamkathan <shethkathan4@gmail.com>
2020-12-18 04:29:25 +0000

    changes done

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ class Sofa(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     description = models.CharField(max_length=1000)
+    
 
     def __str__(self):
         return self.name
